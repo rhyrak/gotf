c15ab0845d23a358b0cc3f0c10a17cf6cc9c0140
Selcuk <sgencay88@gmail.com>
2022-12-15 20:12:46 +0300

    player sprite & anim

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,9 +1,11 @@
 package entities;
 
 import main.Game;
+import util.AssetManager;
 import util.Directions;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 import static util.Directions.*;
 
@@ -14,16 +16,32 @@ public class Player extends Entity {
     private Rectangle attackHitbox;
     private boolean attacking;
     private Directions direction;
+    private BufferedImage[][] sprite;
+    private int animIndex, animTick;
+
 
     public Player(Rectangle hitbox) {
         this.hitbox = hitbox;
         this.attackHitbox = new Rectangle(hitbox.x + hitbox.width, hitbox.y, hitbox.width, hitbox.height);
         this.direction = RIGHT;
+        loadSprite();
     }
 
+
     public void update() {
+        animate();
         move();
         updateAttackHitbox();
+    }
+
+    private void animate() {
+        animTick++;
+        if (animTick >= 50) {
+            animTick = 0;
+            animIndex++;
+            if (animIndex>=4)
+                animIndex = 0;
+        }
     }
 
     private void move() {
@@ -63,6 +81,51 @@ public class Player extends Entity {
     }
 
     public void draw(Graphics g) {
+        drawHitbox(g);
+        switch (direction) {
+            case DOWN -> {
+                if (attacking)
+                    g.drawImage(sprite[4][0], hitbox.x, hitbox.y, hitbox.width, hitbox.height, null);
+                else
+                    g.drawImage(sprite[0][animIndex], hitbox.x, hitbox.y, hitbox.width, hitbox.height, null);
+            }
+            case UP -> {
+                if (attacking)
+                    g.drawImage(sprite[4][1], hitbox.x, hitbox.y, hitbox.width, hitbox.height, null);
+                else
+                    g.drawImage(sprite[1][animIndex], hitbox.x, hitbox.y, hitbox.width, hitbox.height, null);
+            }
+            case LEFT -> {
+                if (attacking)
+                    g.drawImage(sprite[4][2], hitbox.x, hitbox.y, hitbox.width, hitbox.height, null);
+                else
+                    g.drawImage(sprite[2][animIndex], hitbox.x, hitbox.y, hitbox.width, hitbox.height, null);
+            }
+            case RIGHT -> {
+                if (attacking)
+                    g.drawImage(sprite[4][3], hitbox.x, hitbox.y, hitbox.width, hitbox.height, null);
+                else
+                    g.drawImage(sprite[3][animIndex], hitbox.x, hitbox.y, hitbox.width, hitbox.height, null);
+            }
+        }
+    }
+
+    private void loadSprite() {
+        BufferedImage temp = AssetManager.getSprite(AssetManager.PLAYER_SPRITE);
+        sprite = new BufferedImage[8][4];
+        for (int i = 0; i < 4; i++) {
+            sprite[0][i] = temp.getSubimage(0, i * 16, 16, 16); // down
+            sprite[1][i] = temp.getSubimage(16, i * 16, 16, 16); // up
+            sprite[2][i] = temp.getSubimage(32, i * 16, 16, 16); // left
+            sprite[3][i] = temp.getSubimage(48, i * 16, 16, 16); // right
+        }
+        sprite[4][0] = temp.getSubimage(0,64,16,16); // attack down
+        sprite[4][1] = temp.getSubimage(16,64,16,16); // attack up
+        sprite[4][2] = temp.getSubimage(32,64,16,16); // attack left
+        sprite[4][3] = temp.getSubimage(48,64,16,16); // attack right
+    }
+
+    private void drawHitbox(Graphics g) {
         if (Game.DEBUG_MODE) {
             g.drawRect(hitbox.x, hitbox.y, hitbox.width, hitbox.height);
             if (attacking)
--- src/util/AssetManager.java
+++ src/util/AssetManager.java
@@ -10,9 +10,11 @@ public abstract class AssetManager {
     public static final String BUTTONS = "/assets/buttons.png";
     public static final String WINDOWS = "/assets/windows.png";
 
-    public static final String PLAY_BTN = "/ui/menu/start_btn.png";
-    public static final String SETTINGS_BTN = "/ui/menu/settings_btn.png";
-    public static final String EXIT_BTN = "/ui/menu/quit_btn.png";
+    public static final String PLAY_BTN = "/assets/ui/menu/start_btn.png";
+    public static final String SETTINGS_BTN = "/assets/ui/menu/settings_btn.png";
+    public static final String EXIT_BTN = "/assets/ui/menu/quit_btn.png";
+
+    public static final String PLAYER_SPRITE = "/assets/player/player-sprite.png";
 
     public static BufferedImage getSprite(String filePath) {
         BufferedImage image = null;
