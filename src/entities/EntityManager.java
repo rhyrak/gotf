f071819ee298e97752bd5963b98a54b0c529219f
Tayfun Özdemir <113831552+TyfnOz@users.noreply.github.com>
2022-12-28 14:25:42 +0300

    Update EntityManager.java

--- src/entities/EntityManager.java
+++ src/entities/EntityManager.java
@@ -7,20 +7,25 @@ import java.awt.*;
 public class EntityManager {
     private Entity[] entities;
     private Playing state;
-
+    private Player player;
     public EntityManager(Playing playing, Player player) {
         this.state = playing;
         //TODO: Check savedata to find monster count
         //if (playing.getSaveData())
+        this.player = player;
         entities = new Entity[1];
-        entities[0] = new RedNinja(player);
+        entities[0] = new RedNinja(this);
     }
 
     public void update() {
         for(Entity e: entities)
             e.update();
     }
-
+    
+    public Player getPlayer() {
+    	return player;
+    }
+    
     public void draw(Graphics g) {
         for (Entity e: entities)
             e.draw(g);
