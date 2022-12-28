f8e053d0a5dcafee3e5f42eeb1c9b9e442ede4ff
Tayfun Özdemir <113831552+TyfnOz@users.noreply.github.com>
2022-12-28 14:26:04 +0300

    Update Entity.java

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -5,7 +5,8 @@ import java.awt.*;
 public abstract class Entity {
     protected Rectangle hitbox;
     protected int hitpoints;
-
+    protected EntityManager entityManager;
+    
     public abstract void draw(Graphics g);
 
     public abstract void update();
