c91bd5fc77b25ec968ea06ed40b2cc68cd52a7ac
Selcuk <sgencay88@gmail.com>
2022-12-31 12:23:00 +0300

    update EntityManager.java

--- src/entities/EntityManager.java
+++ src/entities/EntityManager.java
@@ -1,54 +1,53 @@
 package entities;
-
 import main.Game;
 import states.Playing;
-
 import java.awt.*;
 import java.util.ArrayList;
 
 public class EntityManager {
 
-    private ArrayList<Entity> entities;
-    private Playing state;
+    private ArrayList<ArrayList<Entity>> entities;
+    private Playing playing;
     private Player player;
     private int camOffsetX, camOffsetY;
 
     public EntityManager(Playing playing, Player player) {
-        this.state = playing;
-        //TODO: Check savedata to find monster count
-        //if (playing.getSaveData())
+        this.playing = playing;
         this.player = player;
         initEntities();
     }
 
     private void initEntities() {
-        entities = new ArrayList<>(20);
-        entities.add(new RedNinja(this, new Rectangle(16*64,16*64,64,64)));
-        entities.add(new Item(this,Item.ItemType.LIFEPOT, 500, 500));
-        entities.add(new Item(this,Item.ItemType.MEDIPACK, 500, 800));
+        //TODO: Check savedata to find monster count
+        entities = new ArrayList<>(5);
+        for (int i = 0; i < 5; i++)
+            entities.add(new ArrayList<>(10));
+        entities.get(1).add(new RedNinja(this, new Rectangle(16*64,16*64,64,64)));
+        entities.get(2).add(new Item(this,Item.ItemType.LIFEPOT, 1000, 1000));
+        entities.get(3).add(new Item(this,Item.ItemType.MEDIPACK, 1000, 1500));
     }
 
     public void update() {
         camOffsetX = Game.gameWidth / 2 - player.getHitbox().x - player.getHitbox().width / 2;
         camOffsetY = Game.gameHeight / 2 - player.getHitbox().y - player.getHitbox().height / 2;
-        for(Entity e: entities)
+        for(Entity e: entities.get(playing.getSaveData().floor))
             e.update();
     }
-    
+
     public Player getPlayer() {
-    	return player;
+        return player;
     }
-    
+
     public void draw(Graphics g) {
-        for (Entity e: entities)
+        for(Entity e: entities.get(playing.getSaveData().floor))
             e.draw(g);
     }
 
     public int getCamOffsetX() {
         return camOffsetX;
     }
-
     public int getCamOffsetY() {
         return camOffsetY;
     }
+
 }
