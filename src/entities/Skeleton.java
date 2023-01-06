dae5fe1a54a90f70a735d4dfc57707ff2c7e4e80
Tayfun Özdemir <113831552+TyfnOz@users.noreply.github.com>
2023-01-06 19:11:29 +0300

    javadoc

--- src/entities/Skeleton.java
+++ src/entities/Skeleton.java
@@ -29,7 +29,12 @@ public class Skeleton extends Entity {
     private boolean isDead = false;
     private BufferedImage[] healthBar;
 
-    // constructor
+    /**
+     * Class constructor 
+     * @param  entityManager  an EntityManager to access player's properties
+     * @param  hitbox  a Rectangle to assign Skeleton's hitbox
+     * @see  EntityManager
+     */
     public Skeleton(EntityManager entityManager, Rectangle hitbox) {
         this.entityManager = entityManager;
         this.hitbox = hitbox;
@@ -40,7 +45,10 @@ public class Skeleton extends Entity {
         startX = hitbox.x;
         startY = hitbox.y;
     }
-
+    
+    /**
+     * Loads needed sprites for Skeleton
+     */
     private void loadSprite() {
         BufferedImage temp = AssetManager.getSprite(AssetManager.SKELETON);
         sprite = new BufferedImage[9][4];
@@ -63,7 +71,9 @@ public class Skeleton extends Entity {
         healthBar[1] = temp.getSubimage(0, 0, 100, 7);
     }
 
-    //checks and sets idle/chase situations
+    /**
+     * Checks and sets idle, chasing, attacking situations
+     */
     public void setAction() {
 
         //reset directions
@@ -147,7 +157,10 @@ public class Skeleton extends Entity {
                 actionLockCounter = 0;
         }
     }
-
+    
+    /**
+     * Sets and resets cool-down time to draw next sprite of current animation
+     */
     private void animate() {
         animTick++;
         if (animTick >= 50) {
@@ -157,7 +170,12 @@ public class Skeleton extends Entity {
                 animIndex = 0;
         }
     }
-
+    
+    /**
+     * Updates the x-axis and y-axis speed according to direction booleans,
+     * then changes the coordinates according to x-axis and y-axis speed,
+     * and then sets the enum Direction according to x-axis and y-axis speed.
+     */
     private void move() {
         int xSpeed = 0, ySpeed = 0;
         int ninjaSpeed = 1;
@@ -181,7 +199,11 @@ public class Skeleton extends Entity {
         else if (xSpeed > 0)
             direction = RIGHT;
     }
-
+    
+    /**
+     * Draws all the sprites and animations, if Skeleton is dead, returns nothing and leaves the function and draws nothing all the time
+     * @param  g  it is a needed instance of Graphics to be able to draw sprites
+     */
     @Override
     public void draw(Graphics g) {
         if (isDead)
@@ -218,12 +240,20 @@ public class Skeleton extends Entity {
             }
         }
     }
-
+    
+    /**
+     * A function to draw health bar above the Skeleton
+     * @param  g  it is a needed instance of Graphics to be able to draw health bar
+     */
     private void drawHealthBar(Graphics g) {
         g.drawImage(healthBar[0], hitbox.x + camOffsetX + 7, hitbox.y + camOffsetY - 15, 50, 4, null);
         g.drawImage(healthBar[1], hitbox.x + camOffsetX + 7, hitbox.y + camOffsetY - 15, hitpoints / 4, 4, null);
     }
-
+    
+    /**
+     * Runs 200 times per second, calls some functions and updates everything.
+     * If Skeleton is dead, returns nothing to leave function. So nothing is updated no more.
+     */
     @Override
     public void update() {
         if (isDead)
@@ -237,7 +267,12 @@ public class Skeleton extends Entity {
         animate();
         move();
     }
-
+    
+    /**
+     * Sets and resets the attacking cooldown.
+     * If Skeleton attacked once, attacking goes on cooldown to prevent to attack for every run of update method (200 attack per second).
+     * After a while, attacking cooldown resets.
+     */
     private void updateCooldowns() {
         //works only if player is always in attack range,
         //if player gets in and out of the attack range repeatedly,
@@ -252,7 +287,10 @@ public class Skeleton extends Entity {
                 attackCoolDown = 0;
         }
     }
-
+    
+    /**
+     * Designates attackHitbox according to direction
+     */
     private void updateAttackHitbox() {
         switch (direction) {
             case UP -> {
@@ -273,7 +311,12 @@ public class Skeleton extends Entity {
             }
         }
     }
-
+    
+    /**
+     * Updates Skeleton's and player's hitpoints if they are attacked.
+     * If Skeleton attacks player, plays the attacking sound and 
+     * sets player's invincible boolean variable to true which prevents player to be attacked repeatedly.
+     */
     private void updateHitpoints() {
         if (attacking && attackHitbox.contains(entityManager.getPlayer().getHitbox()) &&
                 entityManager.getPlayer().isInvincible() == false &&
